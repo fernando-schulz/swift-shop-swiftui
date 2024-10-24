a1d42fcc86d881eac69e51cdb97b7317424da6cc
Fernando Schulz <fernando.rsl3@gmail.com>
2024-10-24 18:38:30 -0300

    Initial Commit

---
+++ SwiftShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftShop
+//
+//  Created by Fernando Schulz on 24/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftShop/SwiftShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShopApp.swift
+//  SwiftShop
+//
+//  Created by Fernando Schulz on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
